cedd02c089d20b6e6a4a5b8e66f1e04914da815d
FELIPE FRIEDRICH SANTOS <631910142 @fspoa.br>
2019-09-04 21:50:00 -0300

    Criada as classes Clientes e Cidades

---
+++ src/Model/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author 631910142
+ */
+public class Cliente {
+    protected int id;
+    protected String nome,email;
+    protected boolean receberEmail;
+    protected Cidade cidade;
+}
---
+++ src/Model/ClientePF.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author 631910142
+ */
+public class ClientePF extends Cliente {
+    
+    private String CPF;
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    
+}
